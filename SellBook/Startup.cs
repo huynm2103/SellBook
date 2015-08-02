882663729dc5332a7a60470dafef6338e9c2fd41
huy nuyen <huynm2103@gmail.com>
2015-08-03 00:46:03 +0700

    first commit

---
+++ SellBook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SellBook.Startup))]
+namespace SellBook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
